c30a33f497f61dc8361d57b5f459c5f552422690
Thiago Macieira <thiagommacieira@gmail.com>
2024-06-16 09:31:24 -0300

    Aprimorando

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ include 'Agendamento/Controller/conexao.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./home.css">
+    <link rel="stylesheet" href="./CSS/home.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <title>EventPro Solution</title>
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.5/css/dataTables.dataTables.css">
